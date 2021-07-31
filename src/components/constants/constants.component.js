30e05c11bba79136873cd00ab507b40ce202ae73
karandeepvirk85 <karandeep.virk@yahoo.com>
2021-07-31 13:05:00 +0530

    Added missing slash

--- src/components/constants/constants.component.js
+++ src/components/constants/constants.component.js
@@ -3,7 +3,7 @@ const newsApi = apiDomain + "/wp-admin/admin-ajax.php?action=get_news";
 const contactController = apiDomain + "/contact_controller.php";
 const blogApi = apiDomain + "/wp-admin/admin-ajax.php?action=get_blogs";
 const categoryBlog =
-    apiDomain + "wp-admin/admin-ajax.php?action=get_blogs&category=";
+    apiDomain + "/wp-admin/admin-ajax.php?action=get_blogs&category=";
 const googleMapAddress =
     "https://www.google.com/maps?q=14373+82b+Ave,+Surrey,+BC+V3W+0J4&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjj8KCqipLrAhWuKDQIHc_LCEMQ_AUoAXoECAwQAw";
 const portfolioEmail = "karandeep.virk@yahoo.com";
